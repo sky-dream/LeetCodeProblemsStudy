058553310005e2da97b01fd28a890568d29dd42f
sky-dream <xxm1263476788@126.com>
2020-02-09 23:46:59 +0800

    update code for problem 621 solution 3.

--- [0621][Medium][Task_Scheduler]/Task_Scheduler_3.java
+++ [0621][Medium][Task_Scheduler]/Task_Scheduler_3.java
@@ -16,3 +16,33 @@ public class Solution {
         return idle_slots > 0 ? idle_slots + tasks.length : tasks.length;
     }
 }
+// solution 3, analysis and design, another version.
+/*
+he ONLY thing you need to care is the max number of one task!
+We set apart each max task with interval n, and we hope to put all other tasks into those intervals. 
+If the number of those tasks exceeds the interval space, then we don't need any idle interval at all. 
+If not, the interval space plus the max tasks will be the least interval. Be care for the existent of multiple max tasks.
+*/
+public class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] storage = new int[26];
+        for (char c : tasks) {
+            storage[(c - 'A')]++;
+        }
+        int max = 0;
+        int count = 1;
+        for (int num : storage) {
+            if (num == 0) {
+                continue;
+            }
+            if (max < num) {
+                max = num;
+                count = 1;
+            } else if (max == num) {
+                count++;
+            }
+        }
+        int space = (n + 1) * (max - 1) + count;
+        return (space < tasks.length) ? tasks.length : space;
+    }
+}
