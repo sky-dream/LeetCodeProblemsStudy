2fb869250d0757e6afe0971f236cbdc72b04b70e
sky-dream <xxm1263476788@126.com>
2020-02-10 01:42:06 +0800

    Update Rearrange_String_k_Distance_Apart.py
    add comments for problem 358.

--- [0358][Hard][Rearrange_String_k_Distance_Apart]/Rearrange_String_k_Distance_Apart.py
+++ [0358][Hard][Rearrange_String_k_Distance_Apart]/Rearrange_String_k_Distance_Apart.py
@@ -20,3 +20,11 @@ class Solution:
             for t in tmp:
                 heapq.heappush(heap, t)
         return res 
+
+#python heapq(堆， 即优先队列PriorityQueue)常用函数             描 述
+#heappush(heap, x)                                              将x压入堆中
+#heappop(heap)                                                  从堆中弹出最小的元素
+#heapify(heap)                                                  让列表具备堆特征
+#heapreplace(heap, x)                                           弹出最小的元素，并将x压入堆中
+#nlargest(n, iter)                                              返回iter中n个最大的元素
+#nsmallest(n, iter)                                             返回iter中n个最小的元素
