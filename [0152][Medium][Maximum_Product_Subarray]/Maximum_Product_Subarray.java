2c37d83752492dc0988e3690eab5f4b1fe70149a
sky-dream <xxm1263476788@126.com>
2020-02-24 00:44:34 +0800

    add code for No.152.

--- [0152][Medium][Maximum_Product_Subarray]/Maximum_Product_Subarray.java
+++ [0152][Medium][Maximum_Product_Subarray]/Maximum_Product_Subarray.java
@@ -18,6 +18,15 @@ class Solution {
         }
         return result;
     }
+    public int maxProduct_1(int[] nums) {
+        int n = nums.length, res = nums[0], l = 0, r = 0;
+        for (int i = 0; i < n; i++) {
+            l =  (l == 0 ? 1 : l) * nums[i];
+            r =  (r == 0 ? 1 : r) * nums[n - 1 - i];
+            res = Math.max(res, Math.max(l, r));
+        }
+        return res;
+    }
 }
 
 public class Maximum_Product_Subarray {
