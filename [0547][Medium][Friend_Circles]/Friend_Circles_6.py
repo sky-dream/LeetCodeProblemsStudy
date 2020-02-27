679b05a2a7fc86af1ddf0f07590b62f191c60b6d
sky-dream <xxm1263476788@126.com>
2020-02-28 02:24:05 +0800

    add py code for No.547.

---
+++ [0547][Medium][Friend_Circles]/Friend_Circles_6.py
@@ -0,0 +1,32 @@
+# solution 1 greedy algorithm.
+# leetcode time     cost : 324 ms
+# leetcode memory   cost : 29.5 MB 
+# Time  Complexity: O(n**2)
+# Space Complexity: O(1)
+# solution 3, Easy Python Union Find
+class Solution(object):
+    def findCircleNum(self, M):
+        def find(x):
+            if x != parents[x]:
+                parents[x] = find(parents[x])
+            return parents[x]    
+        def union(x, y):
+            parents[find(x)] = find(y)
+            
+        n = len(M)
+        parents = list(range(n))
+        for i in range(len(M)):
+            for j in range(len(M[i])):
+                if M[i][j]:
+                    union(i, j)
+        circle = set(find(i) for i in range(n))            
+        return len(circle) 
+    
+def main():
+    M = [[1,1,0],[1,1,0],[0,0,1]]
+    obj = Solution()
+    res = obj.findCircleNum(M)
+    print("return value is ",res)
+    
+if __name__ =='__main__':
+    main()     
