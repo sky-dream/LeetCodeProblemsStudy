dd758d9064453ad80ef1139a3ce93d176ab0f7cc
sky-dream <xxm1263476788@126.com>
2020-03-17 23:54:54 +0800

    update variable name for 685

--- [0685][Hard][Redundant_Connection_II]/Redundant_Connection_II.java
+++ [0685][Hard][Redundant_Connection_II]/Redundant_Connection_II.java
@@ -2,10 +2,10 @@
 // leetcode memory   cost : 39.9 MB
 // https://leetcode.com/problems/redundant-connection-ii/discuss/278105/topic
 class Solution {  
-    int[] anc;//并查集
+    int[] ancester;//并查集
     int[] parent;// record the father of every node to find the one with 2 fathers,记录每个点的父亲，为了找到双入度点
     public int[] findRedundantDirectedConnection(int[][] edges) {
-        anc=new int[edges.length+1];
+        ancester=new int[edges.length+1];
         parent=new int[edges.length+1];
         int[] edge1=null;
         int[] edge2=null;
@@ -14,18 +14,18 @@ class Solution {
             int u=pair[0];
             int v=pair[1];
             
-            if(anc[u]==0) anc[u]=u;
-            if(anc[v]==0) anc[v]=v;//init the union-find set  初始化并查集
+            if(ancester[u]==0) ancester[u]=u;
+            if(ancester[v]==0) ancester[v]=v;//init the union-find set  初始化并查集
                 
             if (parent[v]!=0){// node v already has a father, so we just skip the union of this edge and check if there will be a circle ，跳过 edge2,并记下 edge1,edge2
                 edge1=new int[]{parent[v],v};
                 edge2=pair;
             } else {
                 parent[v]=u;
-                int ancU=find(u);
-                int ancV=find(v);
-                if(ancU!=ancV){
-                    anc[ancV]=ancU;
+                int ancesterU=find(u);
+                int ancesterV=find(v);
+                if(ancesterU!=ancesterV){
+                    ancester[ancesterV]=ancesterU;
                 } else { //meet a circle , 碰到了环
                     lastEdgeCauseCircle=pair;
                 }
@@ -36,8 +36,8 @@ class Solution {
     }
      
     private int find(int node){
-        if (anc[node]==node) return node;
-        anc[node]=find(anc[node]);
-        return anc[node];
+        if (ancester[node]==node) return node;
+        ancester[node]=find(ancester[node]);
+        return ancester[node];
     }
 }
