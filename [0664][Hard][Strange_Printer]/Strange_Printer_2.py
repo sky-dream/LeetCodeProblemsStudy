e03777d3582cceca0f1a2b5e9f37239a2cedbf9c
sky-dream <xxm1263476788@126.com>
2020-04-02 15:14:44 +0800

    update code for 664.

--- [0664][Hard][Strange_Printer]/Strange_Printer_2.py
+++ [0664][Hard][Strange_Printer]/Strange_Printer_2.py
@@ -1,19 +1,23 @@
-# -*- coding: utf-8 -*-  
+# -*- coding: utf-8 -*-
 # leetcode time     cost : 664 ms
 # leetcode memory   cost : 13.9 MB
 # solution 2, interval problem DP
+
+
 class Solution:
-    def strangePrinter(self, s: str) -> int:
+    def strangePrinter(self, s: str):
         if not s:
             return 0
         n = len(s)
         dp = [[0]*(n+1) for i in range(n+1)]
         # dp[l][r], the min print turn for s[l:r+1]
+        # start with the min window interval for every point
         for i in range(1, n+1):
+            # take every point as the left point to check every possible interval
             for l in range(n):
-                # 计算的时候保证小的已经算出来了
+                # make sure the right not go beyound the max range
                 if i + l - 1 < n:
-                    r = l + i -1
+                    r = l + i - 1
                     dp[l][r] = dp[l+1][r] + 1
                     for k in range(l+1, r+1):
                         if s[k] == s[l]:
@@ -25,7 +29,8 @@ def main():
     word1 = "aaabbb"  # ans 2
     obj = Solution()
     res = obj.strangePrinter(word1)
-    print("return value is ",res)
-    
-if __name__ =='__main__':
-    main() 
+    print("return value is ", res)
+
+
+if __name__ == '__main__':
+    main()
