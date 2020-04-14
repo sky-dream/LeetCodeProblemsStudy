f994578e57190f7aa98f5854d3ffa2223d7bb553
sky-dream <xxm1263476788@126.com>
2020-04-14 17:04:45 +0800

    add code for 722.

---
+++ [0722][Medium][Remove_Comments]/Remove_Comments.py
@@ -0,0 +1,28 @@
+# leetcode time     cost : 44 ms
+# leetcode memory   cost : 13.7 MB
+# Time  Complexity: O(N)
+# Space Complexity: O(N)
+class Solution(object):
+    def removeComments(self, source):
+        in_block = False
+        ans = []
+        for line in source:
+            i = 0
+            if not in_block:
+                newline = []
+            while i < len(line):
+                if line[i:i+2] == '/*' and not in_block:
+                    in_block = True
+                    i += 1
+                elif line[i:i+2] == '*/' and in_block:
+                    in_block = False
+                    i += 1
+                elif not in_block and line[i:i+2] == '//':
+                    break
+                elif not in_block:
+                    newline.append(line[i])
+                i += 1
+            if newline and not in_block:
+                ans.append("".join(newline))
+
+        return ans
