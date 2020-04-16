9a7af3c3ed26b8c198ba8ac7c2135f8ff0f1e5ae
sky-dream <xxm1263476788@126.com>
2020-04-16 23:56:02 +0800

    add code for 138.

---
+++ [0138][Medium][Copy_List_with_Random_Pointer]/Copy_List_with_Random_Pointer_2.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-  
+# leetcode time     cost : 44 ms
+# leetcode memory   cost : 14.3 MB
+# Time  Complexity: O(N)
+# Space Complexity: O(N)
+
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+
+# solution 2, iterration
+class Solution(object):
+    def __init__(self):
+        # Creating a visited dictionary to hold old node reference as "key" and new node reference as the "value"
+        self.visited = {}
+
+    def getClonedNode(self, node):
+        # If node exists then
+        if node:
+            # Check if its in the visited dictionary          
+            if node in self.visited:
+                # If its in the visited dictionary then return the new node reference from the dictionary
+                return self.visited[node]
+            else:
+                # Otherwise create a new node, save the reference in the visited dictionary and return it.
+                self.visited[node] = Node(node.val, None, None)
+                return self.visited[node]
+        return None
+
+    def copyRandomList(self, head):
+        """
+        :type head: Node
+        :rtype: Node
+        """
+
+        if not head:
+            return head
+
+        old_node = head
+        # Creating the new head node.       
+        new_node = Node(old_node.val, None, None)
+        self.visited[old_node] = new_node
+
+        # Iterate on the linked list until all nodes are cloned.
+        while old_node != None:
+
+            # Get the clones of the nodes referenced by random and next pointers.
+            new_node.random = self.getClonedNode(old_node.random)
+            new_node.next = self.getClonedNode(old_node.next)
+
+            # Move one step ahead in the linked list.
+            old_node = old_node.next
+            new_node = new_node.next
+
+        return self.visited[head]
