a8923c008734727ea7bb8edf4a02694ae0e58068
sky-dream <xxm1263476788@126.com>
2020-04-25 22:24:42 +0800

    update java and py code for problem 269.

--- [0269][Hard][Alien_Dictionary][Graph_topo_sort]/Alien_Dictionary.java
+++ [0269][Hard][Alien_Dictionary][Graph_topo_sort]/Alien_Dictionary.java
@@ -1,96 +1,125 @@
-// leetcode time     cost : ---- ms
-// leetcode memory   cost : ---- MB
-import java.util.Arrays;
-import java.util.Stack;
-import java.util.HashSet;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.ArrayList;
+// leetcode time     cost : 29 ms
+// leetcode memory   cost : 40.3 MB
 import java.lang.Character;
+import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+//import java.util.Stack;
 class Solution {
-// 基本情况处理，比如输入为空，或者输入的字符串只有一个
-String alienOrder(String[] words) {
-    if (words == null || words.length == 0)
-        return null;
-    if (words.length == 1) {
-        return words[0];
-    }
-  
-    // 构建有向图：定义一个邻接链表 adjList，也可以用邻接矩阵
-    Map<Character, List<Character>> adjList = new HashMap<>();
-
-    for (int i = 0; i < words.length - 1; i++) {
-        String w1 = words[i], w2 = words[i + 1];
-        int n1 = w1.length(), n2 = w2.length();
-
-        boolean found = false;
+    // 基本情况处理，比如输入为空，或者输入的字符串只有一个
+    public String alienOrder(String[] words) {
+        if (words == null || words.length == 0)
+            return null;
+        if (words.length == 1) {
+            return words[0];
+        }
     
-        for (int j = 0; j < Math.max(w1.length(), w2.length()); j++) {
-            Character c1 = j < n1 ? w1.charAt(j) : null;
-            Character c2 = j < n2 ? w2.charAt(j) : null;
+        // 构建有向图：定义一个邻接链表 adjList，也可以用邻接矩阵
+        Map<Character, List<Character>> adjList = new HashMap<>();
 
-            if (c1 != null && !adjList.containsKey(c1)) {
-                adjList.put(c1, new ArrayList<Character>());
-            }
+        for (int i = 0; i < words.length - 1; i++) {
+            String w1 = words[i], w2 = words[i + 1];
+            int n1 = w1.length(), n2 = w2.length();
+            boolean found = false;
+            for (int j = 0; j < Math.max(w1.length(), w2.length()); j++) {
+                Character c1 = j < n1 ? w1.charAt(j) : null;
+                Character c2 = j < n2 ? w2.charAt(j) : null;
 
-            if (c2 != null && !adjList.containsKey(c2)) {
-                adjList.put(c2, new ArrayList<Character>());
-            }
+                if (c1 != null && !adjList.containsKey(c1)) {
+                    adjList.put(c1, new ArrayList<Character>());
+                }
 
-            if (c1 != null && c2 != null && c1 != c2 && !found) {
-                adjList.get(c1).add(c2);
-                found = true;
-            }
-        }
-    }
+                if (c2 != null && !adjList.containsKey(c2)) {
+                    adjList.put(c2, new ArrayList<Character>());
+                }
 
-    Set<Character> visited = new HashSet<>();
-    Set<Character> loop = new HashSet<>();
-    Stack<Character> stack = new Stack<Character>();
-  
-    for (Character key : adjList.keySet()) {
-        if (!visited.contains(key)) {
-            if (!topologicalSort(adjList, key, visited, loop, stack)) {
-                return "";
-            }
-        }
-    }
-
-    StringBuilder sb = new StringBuilder();
-
-    while (!stack.isEmpty()) {
-        sb.append(stack.pop());
-    }
-
-    return sb.toString();
-  
-} 
-        // 将当前节点 u 加入到 visited 集合以及 loop 集合中。
-        boolean topologicalSort(Map<Character, List<Character>> adjList, char u, 
-                                Set<Character> visited, Set<Character> loop, Stack<Character> stack) {
-            visited.add(u);
-            loop.add(u);
-
-            if (adjList.containsKey(u)) {
-                for (int i = 0; i < adjList.get(u).size(); i++) {
-                    char v = adjList.get(u).get(i);
-        
-                    if (loop.contains(v))
-                        return false;
-                
-                    if (!visited.contains(v)) {
-                        if (!topologicalSort(adjList, v, visited, loop, stack)) {
-                            return false;
-                        }
+                if (c1 != null && c2 != null && c1 != c2 && !found) {
+                    //一个char1 可能多次指向 char2, 同一起点和终点只统计一次 
+                    if(!adjList.get(c1).contains(c2)){
+                        adjList.get(c1).add(c2);
                     }
+                    // 字符不等, 说明出现了大于的关系, 记录下来,后面不用看了
+                    found = true;
+                }
+                // used to handle case ["abc","ab"]
+                if(c1 == c2 && (j+1) == n2 && n1>n2 && !found)
+                    return ""; 
+            }
+        }
+        System.out.println("adjList:  "+adjList.toString());
+
+        StringBuilder strBuilder = new StringBuilder();
+        // 拓补排序
+        boolean sortResult = topologicalSort(adjList, strBuilder);
+        if (sortResult){
+            return strBuilder.toString();
+        }
+        else{
+            return ""; 
+        }
+    
+    } 
+    // 拓补排序
+    boolean topologicalSort(Map<Character, List<Character>> adjList,StringBuilder strBuilder) {
+        Queue<Character> nodeHeader = new ArrayDeque<Character>();
+        Map<Character, Integer> indegreeMap = new HashMap<>();
+        //获取 indegree 入度表 
+        for (Character key : adjList.keySet()) {
+            if (!indegreeMap.containsKey(key)) {
+                indegreeMap.put(key, 0);
+            }
+            
+            for (Character c1 : adjList.get(key)) {
+                if (!indegreeMap.containsKey(c1)) {
+                    indegreeMap.put(c1, 1);
+                }
+                else{
+                    int tmp = indegreeMap.get(c1);
+                    indegreeMap.put(c1, tmp+1);
                 }
             }
-            loop.remove(u);
-            stack.push(u);
+        }
+        System.out.println("indegreeMap:  "+indegreeMap.toString());
+        //把所有入度为0的字符节点加入到 Stack nodeHeader
+        for (Character c1 : indegreeMap.keySet()){
+            if (indegreeMap.get(c1)==0){
+                nodeHeader.add(c1);
+            }
+        }
+        //开始拓补排序
+        while(!nodeHeader.isEmpty())
+        {
+            Character header=nodeHeader.poll(); // queue--poll(), stack---pop(),
+            strBuilder.append(header);
+            System.out.print("header: "+header+", ");
+            
+            for(Character c1 : adjList.get(header))
+            {
+                int tmp = indegreeMap.get(c1);
+                if(tmp==1)//如果入度为1,那么header 是最后一个指向 c1的连接
+                {
+                    nodeHeader.add(c1);
+                    indegreeMap.put(c1, 0);
+                }
+                else{
+                    indegreeMap.put(c1, tmp-1);
+                }
+                //System.out.println(",dest is:  "+c1+", indegree value: "+tmp);
+            }
+        }
+        // 如果拓扑排序遍历了所有的字符, 说明有可行解; 
+        // 否则的话说明 1.不是有向无环图, 2.有环, 3.没有可行解
+        if (adjList.keySet().size() == strBuilder.length()){
             return true;
-        }     
+        }
+        else{
+            return false;
+        }
+        
+    }     
 }
 
 public class Alien_Dictionary {
--- [0269][Hard][Alien_Dictionary][Graph_topo_sort]/Alien_Dictionary.py
+++ [0269][Hard][Alien_Dictionary][Graph_topo_sort]/Alien_Dictionary.py
@@ -1,32 +1,46 @@
-# leetcode time     cost : ---- ms
-# leetcode memory   cost : ---- MB 
+# leetcode time     cost : 52 ms
+# leetcode memory   cost : 13.8 MB 
 # Time  Complexity: O(N)
 # Space Complexity: O(N)
 from typing import List
 class Solution:
+    def __init__(self):
+      self.solutionNotFound = False
+      
     def alienOrder(self, words: List[str]) -> str:
+      # init the char mapping table
       dic = self.build_dict(words)
+      print("char mapping dict: ",dic)
+      # create adjacent list
       graph = self.build_adj(words, dic)
+      print("adjacent list: ",graph)
       path = []
-      visited = [0 for _ in range(len(dic))]
-      for i in range(len(dic)):
-        if not visited[i]:
-          state = self.dfs(i, graph, path, visited)
+      # finish topo sort
+      visited = [-1 for _ in range(len(dic))]
+      for k,v in dic.items():
+        if visited[v] is -1:
+          state = self.dfs(v, graph, path, visited)
           if not state: return ""
       path.reverse()
-      temp = {dic[k]:k for k in dic}
-      ans = "".join([temp[i] for i in path])
-      return ans
+      
+      dictIndexToChar = {dic[k]:k for k in dic}
+      ans = "".join([dictIndexToChar[i] for i in path])
+      return ans if not self.solutionNotFound else ""
 
     def dfs(self, start, adj, path, visited):
       visited[start] = 1
       for next_node in adj[start]:
-        if visited[next_node] == 1: return False
+        # =1 means circle found
+        if visited[next_node] == 1: 
+          return False
+        # =2 means this node checked before
         if visited[next_node] == 2: continue
         state = self.dfs(next_node, adj, path, visited)
-        if not state: return False
+        if not state: 
+          return False
       visited[start] = 2
       path.append(start)
+      #print(path,",current start is: ",start)
       return True
     
     def build_dict(self, words):
@@ -43,24 +57,38 @@ class Solution:
       graph = {}
       for i in range(len(dic)):
         graph[i] = []
-      for index, word in enumerate(words):
-        if index < len(words) -1:
-          i = 0
-          while i< len(words[index]) and i < len(words[index+1]) and words[index][i] == words[index+1][i]:
-            i += 1
-          if i< len(words[index]) and i < len(words[index+1]):
-            graph[dic[words[index][i]]].append(dic[words[index+1][i]])
+      
+      for index in range( len(words)-1 ):
+        w1,w2 = words[index],words[index+1]
+        n1,n2 = len(w1),len(w2) 
+        charOrderFound = False
+        for j in range( max(n1,n2) ):
+          c1 = w1[j] if j < n1 else None
+          c2 = w2[j] if j < n2 else None
+          if (c1) and (c2) and (c1 != c2) and (not charOrderFound):
+            charOrderFound = True
+            if dic[c2] not in graph[dic[c1]]:
+              graph[dic[c1]].append(dic[c2])
+          # used to handle case ["abc","ab"]
+          if((c1 == c2) and ((j+1) == n2) and (n1>n2) and (not charOrderFound)):
+            self.solutionNotFound = True
       return graph
 
-def main():
-    words1 = ["ab","abc"]     # expect is "abc"
-    obj = Solution()
-    result = obj.alienOrder(words1)
-    print("last return result is "+str(result));
-    
-    words2 = ["abc","ab"]     # expect is ""
-    obj = Solution()
-    result = obj.alienOrder(words2)
-    print("last return result is "+str(result));
+def main():  
+    words1,expect1 = ["wrt","wrf","er","ett","rftt"],"wertf"
+    words2,expect2 = ["abc","ab"],""
+    words3,expect3 = ["ab","abc"],"abc"
+    words4,expect4 = ["ab","adc"],"abcd"
+    words5,expect5 = ["za","zb","ca","cb"],"abzc"
+    testCases = [(words1,expect1),(words2,expect2),(words3,expect3),(words4,expect4),(words5,expect5)]
+    for input_strs,expectRes in testCases:
+        obj = Solution()
+        print("******************new test start,input is: ",input_strs,"********************")
+        result = obj.alienOrder(input_strs)
+        try:
+            assert result == expectRes
+            print("passed, result is follow expect:",result)
+        except AssertionError as aError:
+            print('failed, result >> ', result,"<< is wrong, ","expect is : "+ expectRes)
 if __name__ =='__main__':
     main() 
