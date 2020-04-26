ec68446a2ae870cba61923225e95f6386ad47a29
sky-dream <xxm1263476788@126.com>
2020-04-27 02:06:26 +0800

    add code for 431 about tree.

---
+++ [0431][Hard][Encode_N-ary_Tree_to_Binary_Tree][Tree]/Encode_N-ary_Tree_to_Binary_Tree_2.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-  
+# leetcode time     cost : 92 ms
+# leetcode memory   cost : 17.5 MB
+# Time  Complexity: O(n)
+# Space Complexity: O(n)
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+# soolution 2, DFS
+class Codec:
+
+    def encode(self, root):
+        """Encodes an n-ary tree to a binary tree.
+        :type root: Node
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+
+        rootNode = TreeNode(root.val)
+        if len(root.children) > 0:
+            firstChild = root.children[0]
+            rootNode.left = self.encode(firstChild)
+
+        # the parent for the rest of the children
+        curr = rootNode.left
+
+        # encode the rest of the children
+        for i in range(1, len(root.children)):
+            curr.right = self.encode(root.children[i])
+            curr = curr.right
+
+        return rootNode
+
+
+    def decode(self, data):
+        """Decodes your binary tree to an n-ary tree.
+        :type data: TreeNode
+        :rtype: Node
+        """
+        if not data:
+            return None
+
+        rootNode = Node(data.val, [])
+
+        curr = data.left
+        while curr:
+            rootNode.children.append(self.decode(curr))
+            curr = curr.right
+
+        return rootNode
