7bb4e2b10dd25d4ceaf8455b93acf001558b41dd
sky-dream <xxm1263476788@126.com>
2020-05-08 00:52:12 +0800

    add code comments for 564 about string.

--- [0564][Hard][Find_the_Closest_Palindrome][String]/Find_the_Closest_Palindrome.java
+++ [0564][Hard][Find_the_Closest_Palindrome][String]/Find_the_Closest_Palindrome.java
@@ -9,7 +9,7 @@ public class Solution {
     public String nearestPalindromic(String n) {
         if (n.equals("1"))
             return "0";
-
+        //'200345'---->'200002'
         String a = mirroring(n);
         long diff1 = Long.MAX_VALUE;
         diff1 = Math.abs(Long.parseLong(n) - Long.parseLong(a));
@@ -18,16 +18,23 @@ public class Solution {
 
         StringBuilder s = new StringBuilder(n);
         int i = (s.length() - 1) / 2;
+        //'200345'---->'199345'
+        //'199345'---->'199991'
         while (i >= 0 && s.charAt(i) == '0') {
             s.replace(i, i + 1, "9");
+            //replace(int start, int end, String str),
+            //使用给定 String 中的字符替换此序列的子字符串中的字符
             i--;
         }
         if (i == 0 && s.charAt(i) == '1') {
+            //public delete(int start, int end),移除此序列的子字符串中的字符
             s.delete(0, 1);
             int mid = (s.length() - 1) / 2;
             s.replace(mid, mid + 1, "9");
         } else
             s.replace(i, i + 1, "" + (char)(s.charAt(i) - 1));
+        //'200345'---->'199345'---->'99345'---->'99399'
+        //'199345'---->'198345'---->'198891'
         String b = mirroring(s.toString());
         long diff2 = Math.abs(Long.parseLong(n) - Long.parseLong(b));
 
@@ -42,6 +49,8 @@ public class Solution {
             s.insert(0, "1");
         } else
             s.replace(i, i + 1, "" + (char)(s.charAt(i) + 1));
+        //'199345'---->'200345'---->'200002'
+        //'200345'---->'200002'
         String c = mirroring(s.toString());
         long diff3 = Math.abs(Long.parseLong(n) - Long.parseLong(c));
 
