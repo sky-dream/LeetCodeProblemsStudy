6dd2b72c0d70fdb7f07fa9ac7e3cd7984f5dcda4
sky-dream <xxm1263476788@126.com>
2020-05-18 01:36:57 +0800

    add code for 17 about backtracking.

---
+++ [0017][Medium][Letter_Combinations_of_a_Phone_Number][BackTracking]/Letter_Combinations_of_a_Phone_Number.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-  
+# solution 1, back tracking
+# Time  Complexity: O(3**m * 4**n),N 是输入数字中对应 3 个字母的数目,M同理，
+# Space Complexity: O(3**m * 4**n)
+# leetcode time     cost : 36 ms
+# leetcode memory   cost : 13.8 MB
+class Solution:
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        phone = {'2': ['a', 'b', 'c'],
+                 '3': ['d', 'e', 'f'],
+                 '4': ['g', 'h', 'i'],
+                 '5': ['j', 'k', 'l'],
+                 '6': ['m', 'n', 'o'],
+                 '7': ['p', 'q', 'r', 's'],
+                 '8': ['t', 'u', 'v'],
+                 '9': ['w', 'x', 'y', 'z']}
+                
+        def backtrack(combination, next_digits):
+            # if there is no more digits to check
+            if len(next_digits) == 0:
+                # the combination is done
+                output.append(combination)
+            # if there are still digits to check
+            else:
+                # iterate over all letters which map 
+                # the next available digit
+                for letter in phone[next_digits[0]]:
+                    # append the current letter to the combination
+                    # and proceed to the next digits
+                    backtrack(combination + letter, next_digits[1:])
+                    
+        output = []
+        if digits:
+            backtrack("", digits)
+        return output
+def main():
+    inputX,expectRes = "23",["ad","ae","af","bd","be","bf","cd","ce","cf"] 
+    obj = Solution()
+    result = obj.letterCombinations(inputX)
+    try:
+        assert result == expectRes
+        print("passed, result is follow expect:",result)
+    except AssertionError as aError:
+        print('failed, result >> ', result,"<< is wrong, ","expect is : "+ expectRes)
+    
+if __name__ =='__main__':
+    main() 
