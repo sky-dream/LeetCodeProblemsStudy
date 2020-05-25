e7ddcb765a82cc82ab5dfadae728d4a1b3599485
sky-dream <xxm1263476788@126.com>
2020-05-26 01:09:30 +0800

    add code for 155 about stack.

---
+++ [0155][Easy][Min_Stack][Stack]/Min_Stack.py
@@ -0,0 +1,30 @@
+# leetcode time     cost : 108 ms
+# leetcode memory   cost : 16.7 MB 
+# Time  Complexity: O(1)
+# Space Complexity: O(N)
+import math
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.min_stack = [math.inf]
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        self.min_stack.append(min(x, self.min_stack[-1]))
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+    
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
